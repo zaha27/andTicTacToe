af6a978d814622a778694293ac47529203ca2055
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-14 23:19:00 +0200

    hardcoded players

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@
 function App() {
   
 
-return (
-      <p>coming soon ...</p>
-    )
+return 
+    <>
+      <main>
+        <div className="container">
+          <ol id = "players">
+            <li>
+              <span className = "player-name">Player 1</span>
+              <span className = "player-symbol">0</span>
+            </li>
+            <li>
+              <span className = "player-name">Player 1</span>
+              <span className = "player-symbol">X</span>
+            </li>
+          </ol>
+        </div>
+
+      </main>
+
+  </>
+    
   }
-  
-  window.App = app 
-
-
-
   export default App
   
