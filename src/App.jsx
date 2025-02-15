9a650131b262e4cabc2c05227cda5945543162e8
zaha27 <andrei.zaharia2005@gmail.com>
2025-02-15 10:44:59 +0200

    button edit for name

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ return (
         <div id="game-container">
           <ol id="players">
             <li>
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">0</span>
+              <span className="player">
+                <span className="player-name">Player 1</span>
+                <span className="player-symbol">0</span>
+              </span>
+              <button>Edit</button>
             </li>
             <li>
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
+              <span className="player">
+                <span className="player-name">Player 1</span>
+                <span className="player-symbol">X</span>
+              </span>
+              <button>Edit</button>
             </li>
           </ol>
         </div>
